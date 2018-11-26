7d2e3d8768f0f97f745428ecfdff18dda47429a5
dpuritt <dpuritt@gmail.com>
2018-11-26 16:40:58 -0500

    Add files via upload

--- Project_2/Project_2/Reward.h
+++ Project_2/Project_2/Reward.h
@@ -2,22 +2,30 @@
 //Daniel Puritt :: 7275218
 //George Agadah : 7405809
 
+
+#include <iostream> 
+using namespace std;
 //Reward header
 #ifndef reward_h
 #define reward_h
 
+ostream& operator<<(ostream &os, const Reward& currentReward) {
+	return (os << currentReward.getReward();
+}
 class Reward {
 		
 	//class variables
 private:
 	int reward;
-
+	friend ostream& operator<<(ostream &os, const Reward& currentReward);
 
 	//constructor(s)
 private:
+	//empty constructor
 	Reward(){
 		reward = 0;
 	}
+	//given value upon creation
 	Reward(int _reward){
 		reward = _reward;
 	}
@@ -25,11 +33,14 @@ private:
 
 public:
 	//method(s)
+	//get private value of reward
 	int getReward() { return reward; }
-	std::ostream &operator<<(std::ostream &os, const Reward& currentReward) { 
+
+	//make reward printable by overriding the << operator
+	ostream& operator<<(ostream &os, const Reward& currentReward) { 
     return (os << currentReward.reward;
 	}
 	//Defined Cast
 	operator int() const { return reward; }
-}
+};
 #endif
